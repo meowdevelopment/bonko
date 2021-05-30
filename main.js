1ab736747f0b96bee9add197b32dd7445b4aae2b
Meow <ez@clap.com>
2021-05-30 02:48:06 +0200

    change file names

---
+++ main.js
@@ -0,0 +1,13 @@
+window._config = {
+	noteLength: 100, // length each instrument should be in the playing state
+	allowZeroDelay: false, // allow note delays of 0
+	defaultDelay: 1000, // default delay for notes
+	waitForNotes: false, // waits for a note to finish the playing state before starting the next delay
+	pushVars: ['_config', '_data', '_util', 'InstrumentEnum', 'InstrumentPerKeyEnum', 'BongoSet', 'BongoSoundboard'], // variables to push to BongoComposer, you shouldnt need to change this
+	forceDark: true, // forces dark theme
+	remote: `${window.location.protocol}//cdn.jsdelivr.net/gh/itzTheMeow/bonko/index.js`, // the remote script to run
+};
+!(async function () {
+	// fetches and runs the script
+	window.eval(await (await fetch(_config.remote)).text());
+})();
